0e9fc66820e0b8b5cc050475df56ae5a9671f5bf
ttowntom <git.running054@passinbox.com>
2024-08-19 11:17:20 +0200

    removes husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,9 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
+	pluginJs.configs.recommended,
 	{
 		languageOptions: { globals: globals.browser },
-		rules: {
-			"no-unused-vars": "warn",
-			"no-undef": "warn",
-		},
+		rules: {},
 	},
-
-	pluginJs.configs.recommended,
 ];
