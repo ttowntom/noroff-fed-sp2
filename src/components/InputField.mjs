03064549a4c4f23a572f490f7aa1e8b550528b6f
ttowntom <git.running054@passinbox.com>
2024-08-23 14:58:18 +0200

    adds input field component

---
+++ src/components/InputField.mjs
@@ -0,0 +1,29 @@
+export default function InputField(label, type) {
+  const container = document.createElement("div");
+  const inputLabel = document.createElement("label");
+  const input = document.createElement("input");
+
+  container.classList.add("flex", "flex-col");
+
+  inputLabel.textContent = label;
+  inputLabel.classList.add("block", "font-semibold", "text-gray-600");
+
+  input.type = type;
+  input.classList.add(
+    "w-full",
+    "px-4",
+    "py-2",
+    "mt-1",
+    "border",
+    "border-battleship",
+    "rounded-md",
+    "focus:outline-none",
+    "focus:ring",
+    "focus:ring-lavender",
+  );
+
+  container.appendChild(inputLabel);
+  container.appendChild(input);
+
+  return container;
+}
