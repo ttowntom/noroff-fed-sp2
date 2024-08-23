4dbe538ca31ec29bd3c6e1d8aea9a35d3ed646b6
ttowntom <git.running054@passinbox.com>
2024-08-23 14:58:00 +0200

    adds button component

---
+++ src/components/Button.mjs
@@ -0,0 +1,25 @@
+export default function Button(icon, type, label) {
+  const button = document.createElement("button");
+  const iconElement = document.createElement("i");
+  const labelElement = document.createElement("span");
+
+  button.type = type;
+  button.classList.add(
+    "px-4",
+    "py-2",
+    "mt-4",
+    "text-white",
+    "bg-lavender",
+    "rounded-md",
+    "hover:bg-lavender-dark",
+    "shadow-md",
+  );
+
+  iconElement.classList.add(`fas`, `fa-${icon}`);
+  labelElement.textContent = label;
+
+  button.appendChild(iconElement);
+  button.appendChild(labelElement);
+
+  return button;
+}
