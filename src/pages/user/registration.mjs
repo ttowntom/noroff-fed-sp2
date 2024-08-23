b4f8b78c6bc47c1e17a4be4257e6f9333d3ff047
ttowntom <git.running054@passinbox.com>
2024-08-23 14:58:50 +0200

    adds registration form

---
+++ src/pages/user/registration.mjs
@@ -0,0 +1,16 @@
+import InputField from "../../components/InputField.mjs";
+import Button from "../../components/Button.mjs";
+
+export default function loadRegistrationPage() {
+  console.log("Registration Page Loaded");
+
+  // Grab the form element
+  const form = document.querySelector(`form`);
+
+  // Append the input fields and button to the form
+  form.appendChild(InputField(`Name`, `text`));
+  form.appendChild(InputField(`Email`, `email`));
+  form.appendChild(InputField(`Password`, `password`));
+
+  form.appendChild(Button(null, `submit`, `Sign Up`));
+}
