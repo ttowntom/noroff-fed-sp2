605108b58cf49eb911d9095eb90f28522bcb158e
ttowntom <git.running054@passinbox.com>
2024-08-26 13:38:53 +0200

    adds xsm screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,14 @@ module.exports = {
         titillium: ["Titillium Web", "sans-serif"],
       },
     },
+    screens: {
+      xsm: "350px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 };
