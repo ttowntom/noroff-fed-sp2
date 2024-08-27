2a4ff288dfacdb51ab2a5bc5e6d9a147163278ea
ttowntom <git.running054@passinbox.com>
2024-08-27 11:34:53 +0200

    button styling

--- src/pages/user/registration.mjs
+++ src/pages/user/registration.mjs
@@ -4,14 +4,14 @@ import { setRegistrationFormListener } from "../../handlers/registration.mjs";
 
 export default function loadRegistrationPage() {
   // Grab the form element
-  const form = document.querySelector(`form`);
+  const form = document.querySelector("form");
 
   // Append the input fields and button to the form
-  form.appendChild(InputField(`Name`, `text`, `name`, true));
-  form.appendChild(InputField(`Email`, `email`, `email`, true));
-  form.appendChild(InputField(`Password`, `password`, `password`, true));
+  form.appendChild(InputField("Name", "text", "name", true));
+  form.appendChild(InputField("Email", "email", "email", true));
+  form.appendChild(InputField("Password", "password", "password", true));
 
-  form.appendChild(Button(null, `submit`, `Sign Up`));
+  form.appendChild(Button(null, "submit", "Sign Up", "lavender", true));
 
   // Add "sign up" event listener to the form
   setRegistrationFormListener();
