b9aaba481bd33f3dbf0698b95b1065b46f73608e
ttowntom <git.running054@passinbox.com>
2024-08-28 19:09:05 +0200

    refactors the Header component

--- src/components/Header.mjs
+++ src/components/Header.mjs
@@ -1,5 +1,7 @@
 import { load } from "../storage/index.mjs";
 import MenuCard from "./MenuCard.mjs";
+import MenuButton from "./MenuButton.mjs";
+import hamburgerMenuToggle from "../handlers/hamburgerMenuToggle.mjs";
 const user = load("profile");
 
 export default function Header() {
@@ -76,115 +78,24 @@ function createActionsColumn() {
   const col3 = document.createElement("nav");
   col3.classList.add("flex", "justify-end", "gap-6", "pt-2");
 
-  let menuButton;
+  // New listing link
   let newListingLink;
   if (user) {
-    newListingLink = createNewListingLink();
+    newListingLink = MenuButton(["fa-circle-plus"], "New listing", null);
     col3.append(newListingLink);
   }
+
+  // Profile link and hamburger menu / log in button
+  let menuButton;
   if (user) {
-    menuButton = createMenuButton();
+    menuButton = MenuButton(["fa-circle-user"], user.name, hamburgerMenuToggle);
   } else {
-    menuButton = document.createElement("a");
-    menuButton.href = "/user/login/";
-    menuButton.innerHTML = createLoginButton();
+    menuButton = MenuButton(["fa-right-to-bracket"], "Log in", () => {
+      window.location.href = "/user/login/";
+    });
   }
 
   col3.append(menuButton);
-  setupMenuToggle(menuButton);
 
   return col3;
-}
-
-function createNewListingLink() {
-  const link = document.createElement("a");
-  link.href = "/listings/new-listing/";
-  link.classList.add(
-    "hidden",
-    "sm:flex",
-    "gap-2",
-    "items-center",
-    "hover:text-opacity-90",
-    "group",
-  );
-  link.innerHTML = `
-    <i class="fa-solid fa-circle-plus text-3xl text-lavender group-hover:text-opacity-90"></i>
-    <p class="font-semibold text-sm text-lavender-dark group-hover:text-opacity-90">New listing</p>
-  `;
-  return link;
-}
-
-function createButton(iconClass, textContent) {
-  const button = document.createElement("button");
-  button.classList.add(
-    "flex",
-    "gap-2",
-    "items-center",
-    "hover:text-opacity-90",
-    "group",
-  );
-
-  const icon = document.createElement("i");
-  icon.classList.add(
-    ...iconClass,
-    "fa-solid",
-    "text-4xl",
-    "sm:text-3xl",
-    "text-lavender",
-    "group-hover:text-opacity-90",
-  );
-
-  const text = document.createElement("p");
-  text.classList.add(
-    "hidden",
-    "sm:flex",
-    "font-semibold",
-    "text-sm",
-    "text-lavender-dark",
-    "group-hover:text-opacity-90",
-  );
-  text.textContent = textContent;
-
-  button.append(icon, text);
-  return button;
-}
-
-function createLoginButton() {
-  const iconClass = ["fa-right-to-bracket"];
-  return createButton(iconClass, "Log in");
-}
-
-function createMenuButton(user) {
-  const iconClass = ["fa-circle-chevron-down"];
-  return createButton(iconClass, "username");
-}
-
-function setupMenuToggle(menuButton) {
-  let menuOpen = false;
-
-  user &&
-    menuButton.addEventListener("click", () => {
-      const menuCard = document.querySelector("#menu-card");
-      const menuIcon = document.querySelector("#menu-icon");
-      menuCard.classList.toggle("hidden");
-      menuIcon.classList.toggle("fa-circle-chevron-down");
-      menuIcon.classList.toggle("fa-circle-xmark");
-      menuOpen = !menuOpen;
-    });
-
-  user &&
-    document.addEventListener("click", (e) => {
-      if (
-        menuOpen &&
-        !e.target.closest("#menu-card") &&
-        !e.target.closest("button")
-      ) {
-        const menuCard = document.querySelector("#menu-card");
-        const menuIcon = document.querySelector("#menu-icon");
-        menuCard.classList.add("hidden");
-        menuIcon.classList.remove("fa-circle-xmark");
-        menuIcon.classList.add("fa-circle-chevron-down");
-        menuOpen = false;
-      }
-    });
 }
