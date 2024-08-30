f3254980efb30f97694ab1d32ff64f73696d044c
ttowntom <git.running054@passinbox.com>
2024-08-30 10:47:54 +0200

    adds user listings and wins

--- src/pages/user/user.mjs
+++ src/pages/user/user.mjs
@@ -1,6 +1,11 @@
 import UserBadge from "../../components/UserBadge.mjs";
+import UserListings from "../../components/UserListings.mjs";
+import UserWins from "../../components/UserWins.mjs";
+
 export default async function loadUserPage() {
   const main = document.querySelector("main");
 
   main.appendChild(await UserBadge());
+  main.appendChild(await UserListings());
+  main.appendChild(await UserWins());
 }
