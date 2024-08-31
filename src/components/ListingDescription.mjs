50a9f2ae234a7057f3370b587f3663fe0252e6c0
ttowntom <git.running054@passinbox.com>
2024-08-31 10:57:24 +0200

    adds markdown formatting

---
+++ src/components/ListingDescription.mjs
@@ -0,0 +1,24 @@
+export default function ListingDescription(listing) {
+  const container = document.createElement("div");
+  container.classList.add("flex", "flex-col", "flex-grow", "gap-4");
+
+  const titleEl = document.createElement("h2");
+  titleEl.textContent = listing.data.title;
+  titleEl.classList.add(
+    "text-2xl",
+    "font-semibold",
+    "text-lavender-dark",
+    "font-rowdies",
+  );
+
+  const descriptionHTML = marked.parse(listing.data.description);
+  const descriptionEl = document.createElement("div");
+  descriptionEl.innerHTML = descriptionHTML;
+  // const descriptionEl = document.createElement("p");
+  // descriptionEl.textContent = listing.data.description;
+
+  container.appendChild(titleEl);
+  container.appendChild(descriptionEl);
+
+  return container;
+}
