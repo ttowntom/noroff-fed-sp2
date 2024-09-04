9553b8961d0f59d2105dd7fab67ab98b655688c9
ttowntom <torgeir.riseth@ttowntom.com>
2024-09-04 11:48:17 +0200

    adds modal content elements

--- src/components/BidButton.mjs
+++ src/components/BidButton.mjs
@@ -3,30 +3,6 @@ import BidModal from "./BidModal.mjs";
 
 const user = load("profile")?.name || false;
 
-const bidButton = document.createElement("button");
-bidButton.textContent = "Bid";
-bidButton.classList.add(
-  "w-4/5",
-  "text-white",
-  "font-bold",
-  "uppercase",
-  "rounded-md",
-  "py-2",
-  "px-4",
-  "my-4",
-  "shadow-xl",
-  "bg-gradient-to-t",
-  "from-golf",
-  "to-[#00A8AA]",
-  "hover:from-golf",
-  "hover:to-golf",
-);
-
-bidButton.addEventListener("click", () => {
-  const modal = BidModal();
-  document.body.appendChild(modal);
-});
-
 const ownerNotice = document.createElement("p");
 ownerNotice.textContent = "You can't bid on your own item";
 
@@ -38,6 +14,30 @@ loginNotice.classList.add("text-lavender-dark", "hover:underline");
 export default function BidButton(listing) {
   const seller = listing.seller.name;
   const isSeller = user === seller;
+
+  const bidButton = document.createElement("button");
+  bidButton.textContent = "Bid";
+  bidButton.classList.add(
+    "w-4/5",
+    "text-white",
+    "font-bold",
+    "uppercase",
+    "rounded-md",
+    "py-2",
+    "px-4",
+    "my-4",
+    "shadow-xl",
+    "bg-gradient-to-t",
+    "from-golf",
+    "to-[#00A8AA]",
+    "hover:from-golf",
+    "hover:to-golf",
+  );
+
+  bidButton.addEventListener("click", () => {
+    const modal = BidModal(listing);
+    document.body.appendChild(modal);
+  });
 
   if (user && !isSeller) {
     return bidButton;
--- src/components/BidModal.mjs
+++ src/components/BidModal.mjs
@@ -1,4 +1,7 @@
-export default function BidModal() {
+import InputField from "./InputField.mjs";
+import BidButton from "./BidButton.mjs";
+
+export default function BidModal(listing) {
   // Create modal overlay
   const modalOverlay = document.createElement("div");
   modalOverlay.id = "modal-overlay";
@@ -14,12 +17,22 @@ export default function BidModal() {
     "bg-black",
     "bg-opacity-50",
     "z-50",
+    "shadow-lg",
   );
 
   // Create modal content
   const modalContent = document.createElement("div");
   modalContent.id = "modal-content";
-  modalContent.classList.add("bg-lavender-dark", "p-6", "rounded-md");
+  modalContent.classList.add(
+    "w-5/6",
+    "sm:max-w-sm",
+    "flex",
+    "flex-col",
+    "items-center",
+    "bg-lavender-dark",
+    "p-6",
+    "rounded-md",
+  );
 
   // Create close button
   const closeButton = document.createElement("button");
@@ -34,20 +47,63 @@ export default function BidModal() {
     "cursor-pointer",
   );
 
+  // Create title
+  const title = document.createElement("h2");
+  title.textContent = "Place your bid";
+  title.classList.add(
+    "text-white",
+    "text-2xl",
+    "font-bold",
+    "mb-6",
+    "text-rowdies",
+  );
+  modalContent.appendChild(title);
+
+  // Create credits notice
+  const notice = document.createElement("div");
+  notice.classList.add(
+    "w-full",
+    "flex",
+    "gap-2",
+    "items-center",
+    "justify-center",
+    "text-lavender-dark",
+    "mb-4",
+    "bg-white",
+    "p-1",
+    "rounded-md",
+  );
+
+  const noticeIcon = document.createElement("i");
+  noticeIcon.classList.add("fa-solid", "fa-dollar-sign", "text-lg");
+  notice.appendChild(noticeIcon);
+
+  const noticeText = document.createElement("p");
+  noticeText.textContent = "1 000";
+  noticeText.classList.add("font-semibold", "text-center");
+  const textSpan = document.createElement("span");
+  textSpan.textContent = "credits available";
+  textSpan.classList.add("block", "-mt-2", "font-thin");
+  noticeText.appendChild(textSpan);
+  notice.appendChild(noticeText);
+
+  modalContent.appendChild(notice);
+
   // Create form
   const form = document.createElement("form");
   form.id = "bid-form";
-  form.classList.add("flex", "flex-col", "space-y-4");
-  form.innerHTML = `
-    <h3>Place Your Bid</h3>
-    <label for="bid-amount">Bid Amount:</label>
-    <input type="number" id="bid-amount" name="bidAmount" required>
-    <button type="submit">Submit Bid</button>
-  `;
+  form.classList.add("w-full", "flex", "flex-col", "space-y-4");
+
+  // Create input fields
+  const bidField = InputField(null, "number", "bid", true, false, "Bid amount");
+  bidField.id = "bid-amount";
+  bidField.classList.add("flex-grow");
+  form.appendChild(bidField);
 
   // Append elements to modal content
   modalContent.appendChild(closeButton);
   modalContent.appendChild(form);
+  modalContent.appendChild(BidButton(listing));
 
   // Append modal content to overlay
   modalOverlay.appendChild(modalContent);
