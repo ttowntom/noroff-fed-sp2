6a111fbb0397b5bd449dde5e6af40260faadef47
ttowntom <git.running054@passinbox.com>
2024-09-10 08:47:40 +0200

    adds h1

--- src/pages/user/user.mjs
+++ src/pages/user/user.mjs
@@ -5,13 +5,23 @@ import UserWins from "../../components/UserWins.mjs";
 import { load } from "../../storage/index.mjs";
 const user = load("profile")?.name || null;
 
+// Get URL parameters
+const urlParams = new URLSearchParams(window.location.search);
+const userName = urlParams.get("name");
+
 export default async function loadUserPage() {
   if (!user) {
     window.location.href = "/user/login/";
     return;
   }
 
+  // Grab main
   const main = document.querySelector("main");
+  main.ariaLive = "polite";
+  const title = document.createElement("h1");
+  title.classList.add("sr-only");
+  title.textContent = userName;
+  main.appendChild(title);
 
   main.appendChild(await UserBadge());
   main.appendChild(await UserListings());
