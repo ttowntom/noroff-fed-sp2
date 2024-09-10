ec7022f51ddc5bba0ec6c874ddf819c450686523
ttowntom <git.running054@passinbox.com>
2024-09-10 08:02:07 +0200

    re-directs if not logged in

--- src/pages/user/user.mjs
+++ src/pages/user/user.mjs
@@ -2,7 +2,15 @@ import UserBadge from "../../components/UserBadge.mjs";
 import UserListings from "../../components/UserListings.mjs";
 import UserWins from "../../components/UserWins.mjs";
 
+import { load } from "../../storage/index.mjs";
+const user = load("profile")?.name || null;
+
 export default async function loadUserPage() {
+  if (!user) {
+    window.location.href = "/user/login/";
+    return;
+  }
+
   const main = document.querySelector("main");
 
   main.appendChild(await UserBadge());
