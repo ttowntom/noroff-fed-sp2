3442c5f26191f7f355fe074f5b06663d540b5a93
ttowntom <git.running054@passinbox.com>
2024-09-10 12:27:24 +0200

    refactor: Improve router path handling for better user experience

--- src/router.mjs
+++ src/router.mjs
@@ -16,26 +16,37 @@ export default function router() {
 
   switch (path) {
     case "/":
+    case "/index.html":
       // Listings page
       loadListingsPage();
       break;
     case "/user/sign-up/":
+    case "/user/sign-up":
+    case "/user/sign-up/index.html":
       // Registration page
       loadRegistrationPage();
       break;
     case "/user/login/":
+    case "/user/login":
+    case "/user/login/index.html":
       // Login page
       loadLoginPage();
       break;
     case "/listings/":
+    case "/listings":
+    case "/listings/index.html":
       // Listing page
       loadListingPage();
       break;
     case "/listings/search/":
+    case "/listings/search":
+    case "/listings/search/index.html":
       // Listing page
       loadSearchListingsPage();
       break;
     case "/listings/new-listing/":
+    case "/listings/new-listing":
+    case "/listings/new-listing/index.html":
       // Redirect to login page using window.location
       if (!isUserLoggedIn) {
         window.location.href = "/user/login/";
@@ -44,6 +55,8 @@ export default function router() {
       loadNewListingPage();
       break;
     case "/listings/edit-listing/":
+    case "/listings/edit-listing":
+    case "/listings/edit-listing/index.html":
       // Redirect to login page using window.location
       if (!isUserLoggedIn) {
         window.location.href = "/user/login/";
@@ -52,6 +65,8 @@ export default function router() {
       loadEditListingPage();
       break;
     case "/user/":
+    case "/user":
+    case "/user/index.html":
       // Redirect to login page using window.location
       if (!isUserLoggedIn) {
         window.location.href = "/user/login/";
