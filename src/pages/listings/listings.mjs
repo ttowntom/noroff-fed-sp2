b17618b028429592f16cd06ded6505a44bb662d6
ttowntom <git.running054@passinbox.com>
2024-09-10 08:40:39 +0200

    sr friendly

--- src/pages/listings/listings.mjs
+++ src/pages/listings/listings.mjs
@@ -18,14 +18,20 @@ async function triggerInfiniteScroll() {
 export default async function loadListingsPage() {
   // Grab main
   const main = document.querySelector("main");
+  main.ariaLive = "polite";
   const title = document.createElement("h1");
   title.classList.add("sr-only");
   title.textContent = "Home";
   main.appendChild(title);
 
   // Add search bar
+  const searchTitle = document.createElement("h2");
+  searchTitle.classList.add("sr-only");
+  searchTitle.textContent = "Search listings";
+  main.appendChild(searchTitle);
   main.appendChild(SearchBar());
 
+  // Add infinite scroll event listener
   window.addEventListener("scroll", async () => {
     if (isScrollNearBottom()) {
       triggerInfiniteScroll();
