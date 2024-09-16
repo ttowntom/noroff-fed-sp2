56f895a7520ed93ef8649b38eafef6b4e89d2d6c
ttowntom <git.running054@passinbox.com>
2024-09-16 13:04:05 +0200

    bugfix: selector

--- src/handlers/hamburgerMenuToggle.mjs
+++ src/handlers/hamburgerMenuToggle.mjs
@@ -3,29 +3,57 @@ const user = load("profile");
 
 export default function hamburgerMenuToggle() {
   let menuOpen = false;
+  const btn = document.querySelector(`#btn-${user.name.toLowerCase()}`);
 
   if (user) {
     const menuCard = document.querySelector("#menu-card");
-    const menuIcon = document.querySelector(`#btn-${user.name} i`);
-    menuCard.classList.toggle("hidden");
-    menuIcon.classList.toggle("fa-circle-user");
-    menuIcon.classList.toggle("fa-circle-chevron-down");
+    const menuIcon = document.querySelector(
+      `#btn-${user.name.toLowerCase()} i`
+    );
+
+    menuCard.classList.remove("hidden");
+    menuIcon.classList.remove("fa-circle-user");
+    menuIcon.classList.add("fa-circle-chevron-down");
     menuOpen = !menuOpen;
   }
 
+  function closeMenu(e) {
+    if (
+      menuOpen &&
+      !e.target.closest("#menu-card") &&
+      !e.target.closest("button")
+    ) {
+      const menuCard = document.querySelector("#menu-card");
+      const menuIcon = document.querySelector(
+        `#btn-${user.name.toLowerCase()} i`
+      );
+      menuCard.classList.add("hidden");
+      menuIcon.classList.remove("fa-circle-chevron-down");
+      menuIcon.classList.add("fa-circle-user");
+      menuOpen = false;
+    }
+  }
+
+  user && document.addEventListener("click", (e) => closeMenu(e));
+
+  // Close menu when user clicks on the button
   user &&
-    document.addEventListener("click", (e) => {
-      if (
-        menuOpen &&
-        !e.target.closest("#menu-card") &&
-        !e.target.closest("button")
-      ) {
-        const menuCard = document.querySelector("#menu-card");
-        const menuIcon = document.querySelector(`#btn-${user.name} i`);
+    btn.addEventListener("click", () => {
+      const menuCard = document.querySelector("#menu-card");
+      const menuIcon = document.querySelector(
+        `#btn-${user.name.toLowerCase()} i`
+      );
+
+      if (menuOpen) {
         menuCard.classList.add("hidden");
         menuIcon.classList.remove("fa-circle-chevron-down");
         menuIcon.classList.add("fa-circle-user");
-        menuOpen = false;
+      } else {
+        menuCard.classList.remove("hidden");
+        menuIcon.classList.remove("fa-circle-user");
+        menuIcon.classList.add("fa-circle-chevron-down");
       }
+
+      menuOpen = false;
     });
 }
